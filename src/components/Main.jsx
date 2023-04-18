55326e60279b92c99e4dc4ffde399d51d492cd44
Cmauricio Parra <cmauriciop@yahoo.com.mx>
2023-04-18 13:00:37 -0500

    Add: citizens hook, logic getToken, getCitizens and sending props to citizens components

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,10 +1,53 @@
+import { useEffect, useState } from 'react'
+import axios from '../axios'
+
 import CitizenForm from './CitizenForm'
 import CitizenList from './CitizenList'
+
 import './Main.css'
 
 export default function Main() {
+  const [token, setToken] = useState('')
+  const [citizens, setCitizens] = useState([]) 
+
+  const getToken = () => {
+    axios.post('/auth', {
+      username: 'sarah',
+      password: 'connor'
+    }).then(res => setToken(res.data.token))
+    .catch(err => alert(`${err.message}, please check your connection`))
+  }
+
+  const getCitizens = token => {
+    const config = {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    }
+
+    if (token) {
+      axios.get('/api/members', config).then(res => {
+        setCitizens(res.data)
+      }).catch(err => console.log(err))
+    }
+  }
+
+  useEffect(() => {
+    getToken()
+  }, [])
+
+  useEffect(() => {
+    // get Citizens every 2 secs
+    const intervalId = setInterval(getCitizens, 2000, token)
+    return () => clearInterval(intervalId)
+  }, [])
+
+  useEffect(() => {
+    getCitizens(token)
+  }, [token])
+
   return <main>
-    <CitizenForm />
-    <CitizenList />
+    <CitizenForm token={token} citizens={citizens} setCitizens={setCitizens} />
+    <CitizenList citizens={citizens} />
   </main>
 }
