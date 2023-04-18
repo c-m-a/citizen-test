3c4c64d8ca7accaebb46ae856026fcb869309594
Cmauricio Parra <cmauriciop@yahoo.com.mx>
2023-04-18 08:19:33 -0500

    Add: logic to validate fields in the citizen form component

--- src/components/CitizenForm.jsx
+++ src/components/CitizenForm.jsx
@@ -1,17 +1,20 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './CitizenForm.css'
 
 export default function CitizenForm() {
+  const [disabled, setDisabled] = useState(true)
+
   const [citizen, setCitizen] = useState({
     firstName: '',
     lastName: '',
     address: '',
     ssn: ''
   })
+  const [errors, setErrors] = useState({})
 
   const { firstName, lastName, address, ssn } = citizen
 
-  const changeValue = e => {
+  const handleChange = e => {
     const key = e.target.name
     const value = e.target.value
 
@@ -33,17 +36,74 @@ export default function CitizenForm() {
 
   const saveCitizen = e => {
     e.preventDefault()
+    console.log('save')
   }
 
+  const validate = () => {
+    const ssnRe = /\d{3}\-\d{2}\-\d{4}/
+    let errors = {}
+
+    if (firstName.trim().length === 0)
+      errors.firstName = 'First Name is required'
+    
+    if (lastName.trim().length === 0)
+      errors.lastName = 'Last Name is required'
+
+    if (address.trim().length === 0)
+      errors.address = 'Address is required'
+
+    if (ssnRe.exec(ssn.trim()) === null)
+      errors.ssn = 'SSN needs to be on the follow format: 111-22-4444'
+
+    const isValid = Object.keys(errors).length === 0 && errors.constructor === Object
+
+    return [ isValid, errors ]
+  }
+
+  const handleSubmit = e => {
+
+  }
+
+  useEffect(() => {
+    const [ isValid, errorMsgs ] = validate()
+
+    setDisabled(!isValid)
+    setErrors(prevErrors => ({ ...prevErrors, ...errorMsgs }))
+  }, [citizen])
+
   return <div className="citizenContainer">
-    <form className="citizenForm">
-      <input type="text" name="firstName" value={firstName} onChange={e => changeValue(e)} placeholder="First Name" />
-      <input type="text" name="lastName" value={lastName} onChange={e => changeValue(e)} placeholder="Last Name" />
-      <input type="text" name="address" value={address} onChange={e => changeValue(e)} placeholder="Address" />
-      <input type="text" name="ssn" value={ssn} onChange={e => changeValue(e)} placeholder="SSN" />
+    <form className="citizenForm" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        name="firstName"
+        value={firstName}
+        onChange={handleChange}
+        placeholder="First Name"
+      />
+      <input
+        type="text"
+        name="lastName"
+        value={lastName}
+        onChange={handleChange}
+        placeholder="Last Name"
+      />
+      <input
+        type="text"
+        name="address"
+        value={address}
+        onChange={handleChange}
+        placeholder="Address" />
+      <input
+        type="text"
+        name="ssn"
+        maxLength="11"
+        value={ssn}
+        onChange={handleChange}
+        placeholder="SSN format (###-##-####)"
+      />
       <div className="citizenButtons">
-        <button onClick={e => resetCitizen(e)}>Reset</button>
-        <button onClick={e => saveCitizen(e)}>Save</button>
+        <button onClick={resetCitizen}>Reset</button>
+        <button onClick={saveCitizen} disabled={disabled}>Save</button>
       </div>
     </form>
   </div>
