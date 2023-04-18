a0289ecca330dbfc657ff4d41858a9ae717633e2
Cmauricio Parra <cmauriciop@yahoo.com.mx>
2023-04-17 19:57:15 -0500

    Add: Styles to Citizen Form component and reset and update logic

--- src/components/CitizenForm.jsx
+++ src/components/CitizenForm.jsx
@@ -1,13 +1,49 @@
+import { useState } from 'react'
+import './CitizenForm.css'
+
 export default function CitizenForm() {
+  const [citizen, setCitizen] = useState({
+    firstName: '',
+    lastName: '',
+    address: '',
+    ssn: ''
+  })
+
+  const { firstName, lastName, address, ssn } = citizen
+
+  const changeValue = e => {
+    const key = e.target.name
+    const value = e.target.value
+
+    setCitizen(prevCitizen => ({
+      ...prevCitizen,
+      [key]: value,
+    }))
+  }
+
+  const resetCitizen = e => {
+    e.preventDefault()
+    setCitizen(() => ({
+      firstName: '',
+      lastName: '',
+      address: '',
+      ssn: ''
+    }))
+  }
+
+  const saveCitizen = e => {
+    e.preventDefault()
+  }
+
   return <div className="citizenContainer">
     <form className="citizenForm">
-      <input type="text" name="firstName" value="" placeholder="First Name" />
-      <input type="text" name="lastName" value="" placeholder="Last Name" />
-      <input type="text" name="address" value="" placeholder="Address" />
-      <input type="text" name="ssn" value="" placeholder="SSN" />
-      <div class="citizenFormButtons">
-        <button>Reset</button>
-        <button>Save</button>
+      <input type="text" name="firstName" value={firstName} onChange={e => changeValue(e)} placeholder="First Name" />
+      <input type="text" name="lastName" value={lastName} onChange={e => changeValue(e)} placeholder="Last Name" />
+      <input type="text" name="address" value={address} onChange={e => changeValue(e)} placeholder="Address" />
+      <input type="text" name="ssn" value={ssn} onChange={e => changeValue(e)} placeholder="SSN" />
+      <div className="citizenButtons">
+        <button onClick={e => resetCitizen(e)}>Reset</button>
+        <button onClick={e => saveCitizen(e)}>Save</button>
       </div>
     </form>
   </div>
