61a1091981ee745974dc898c2cbd1646c504e171
Cmauricio Parra <cmauriciop@yahoo.com.mx>
2023-04-17 19:58:35 -0500

    Add: thead and tbody tags to Citizen List component

--- src/components/CitizenList.jsx
+++ src/components/CitizenList.jsx
@@ -1,18 +1,22 @@
 export default function CitizenList() {
   return <div>
     <table>
-      <tr>
-        <th>First Name</th>
-        <th>Last Name</th>
-        <th>Address</th>
-        <th>SSN</th>
-      </tr>
-      <tr>
-        <td>1</td>
-        <td>1</td>
-        <td>1</td>
-        <td>1</td>
-      </tr>
+      <thead>
+        <tr>
+          <th>First Name</th>
+          <th>Last Name</th>
+          <th>Address</th>
+          <th>SSN</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>1</td>
+          <td>1</td>
+          <td>1</td>
+          <td>1</td>
+        </tr>
+      </tbody>
     </table>
   </div> 
 }
